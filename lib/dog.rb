194799f5d779539ec67d2b895d628e659830cc95
Keenah Vancampenhout <kvancamp@mail.com>
2020-08-26 19:39:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,7 @@
 class Dog
-  def initialize(name)
+  def initialize(name, breed = "Mut")
     @name = name
+    @breed = breed
   end
-  def breed=(breed)
-    @breed = Mutt
-  end
-
-
-
-
 end
+fido = Dog.new("Fido")
